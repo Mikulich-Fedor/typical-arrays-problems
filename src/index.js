02571e13418048ebe1dbb8448fb03033dd8bc165
fiodar miculich <feotheodor@gmail.com>
2020-10-03 20:57:38 +0300

    init: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array){
+  if(Array.isArray(array) && array.length>0){
+    return array.reduce((min, item)=>Math.min(min,item));
+  }else return 0 
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array){
+  if(Array.isArray(array) && array.length>0){
+    return array.reduce((max,item)=>Math.max(max,item));
+  }else return 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array){
+  if(Array.isArray(array)&&array.length>0){
+    return array.reduce((sum,item)=>sum+item)/array.length;
+  }else return 0
 }
